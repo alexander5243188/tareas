844431d694fafa2426921b7fef5b6d2c1b9d3fb4
alexander5243188 <alexander5243188@yahoo.es>
2020-10-28 00:07:23 -0400

    Update email.js
    configuración de mailtrap

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: '',
-    pass: '', 
-    host: '',
-    port: ''
+    user: 'c3d6a668bb3d71',
+    pass: 'd97410bea79b1d', 
+    host: 'smtp.mailtrap.io',
+    port: '2525'
 }
