1825eb3af01163600c6ffc177c9c256db3665342
alexander5243188 <alexander5243188@yahoo.es>
2020-10-28 10:09:04 -0400

    update db.js
    Corrección en las variables de conexión

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ require('dotenv').config({ path:'variables.env'})
 
 const db = new Sequelize(
     process.env.BD_NOMBRE, 
-    process.env.DB_USER,
-    process.env.DB_PASS, {
+    process.env.BD_USER,
+    process.env.BD_PASS, {
   host: process.env.BD_HOST,
   dialect: 'mysql',
   port: process.env.BD_PORT,
